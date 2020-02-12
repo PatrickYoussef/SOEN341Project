738a76274c08acd449cba7eed5fab77aaf71b567
Patrick <patrick_youssef@hotmail.com>
2020-02-12 01:54:50 -0500

    Added bootstrap

---
+++ StockHut/StockHut/Models/Options.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StockHut.Models
+{
+    public partial class Options
+    {
+        public int? PhotoId { get; set; }
+        public int? FollowId { get; set; }
+        public int? FollowerId { get; set; }
+    }
+}
---
+++ StockHut/StockHut/Models/StockHutContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace StockHut.Models
+{
+    public partial class StockHutContext : DbContext
+    {
+        public StockHutContext()
+        {
+        }
+
+        public StockHutContext(DbContextOptions<StockHutContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Options> Options { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=stockhut.database.windows.net;Database=StockHut;User Id=PatrickYoussef;Password=Concordia.10");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Options>(entity =>
+            {
+                entity.HasNoKey();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
