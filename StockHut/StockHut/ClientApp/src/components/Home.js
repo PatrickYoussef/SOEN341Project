5fad9160d8b625d7f1cc75273981a0ff4e976d79
d-rossi <david.rossi101@gmail.com>
2020-02-13 17:32:47 -0500

    Soen341_215 Created The NavBar
    Created The NavBar

--- StockHut/StockHut/ClientApp/src/components/Home.js
+++ StockHut/StockHut/ClientApp/src/components/Home.js
@@ -1,30 +1,31 @@
-import React, { Component } from 'react';
-import logo from "../login/stockhut.png"
-import "../login/login.css"
-import "../login/signup.js"
+import React, { Component } from "react";
+import logo from "../login/stockhut.png";
+import "../login/login.css";
+import "../login/signup.js";
 
 const Home = () => {
-
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <div className="login-page" style={{ backgroundColor: "rgb(199, 255, 171)" }}>
-                    <div className="form">
-
-                        <form className="login-form" >
-                            <input type="text" placeholder="username" />
-                            <input type="password" placeholder="password" />
-                            <button>login</button>
-                            <p className="message"><a href="signup.js">Create an account</a></p>
-                        </form>
-
-                    </div>
-                </div>
-
-            </header>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div
+          className="login-page"
+          style={{ backgroundColor: "rgb(199, 255, 171)" }}
+        >
+          <div className="form">
+            <form className="login-form">
+              <input type="text" placeholder="username" />
+              <input type="password" placeholder="password" />
+              <button>login</button>
+              <p className="message">
+                <a href="signup.js">Create an account</a>
+              </p>
+            </form>
+          </div>
         </div>
-    
-}
+      </header>
+    </div>
+  );
+};
 
-export default Home
-
+export default Home;
---
+++ StockHut/StockHut/ClientApp/src/components/NavBar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+import TemporaryDrawer from "./NavDrawer";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
+function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar style={{ backgroundColor: "green" }}>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <TemporaryDrawer />
+          </IconButton>
+          <Typography variant="h6" className={classes.title} align="center">
+            StockHut
+          </Typography>
+          <Button color="inherit">
+            <strong>Login</strong>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default ButtonAppBar;
