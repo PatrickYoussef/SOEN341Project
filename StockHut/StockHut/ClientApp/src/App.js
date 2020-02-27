794d617ce658c724a2d536957b020c597ea33fb0
jenniferasmar <60263256+jenniferasmar@users.noreply.github.com>
2020-02-26 22:45:56 -0500

    Update App.js

--- StockHut/StockHut/ClientApp/src/App.js
+++ StockHut/StockHut/ClientApp/src/App.js
@@ -4,8 +4,12 @@ import { Layout } from './components/Layout';
 import Home from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+<<<<<<< Updated upstream
 import { Signup } from './components/Signup';
 import { NewsFeed } from './components/NewsFeed';
+=======
+import { StreamApp, NotificationDropdown } from 'react-activity-feed';
+>>>>>>> Stashed changes
 import './custom.css'
 
 export default class App extends Component {
@@ -13,6 +17,7 @@ export default class App extends Component {
 
   render () {
     return (
+<<<<<<< Updated upstream
         <Layout>
             <Route exact path='/' component={Home} />
             <Route path='/counter' component={Counter} />
@@ -20,6 +25,20 @@ export default class App extends Component {
             <Route path='/signup' component={Signup} />
             <Route path='/newsfeed' component={NewsFeed} />
         </Layout>
+=======
+        <StreamApp
+            apiKey="du8he7epvp94"
+            appId="45206"
+            token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiN2Q5Y2U5YzQtNGMxNy00MmI5LTk2MTQtMjlkYTM5MTI2M2ZkIn0.3uwnXzk9OHEchMoEzhDGKvGAuFN6YashNhYaMMvSrqY">
+            <Layout>
+                <Route exact path='/' component={Home} />
+                <Route path='/counter' component={Counter} />
+                <Route path='/fetch-data' component={FetchData} />
+            </Layout>
+            <NotificationDropdown notify />
+        </StreamApp>
+      
+>>>>>>> Stashed changes
        
     );
   }
