c83ea8791e10b2b72d4c2303e5674fc388f7da26
Lorenzo001 <34139780+Lorenzo001@users.noreply.github.com>
2020-04-02 16:49:36 -0400

    Create the contactUs page
    You can now reach us via our contactus page an email will be sent to our team.

--- StockHut/StockHut/ClientApp/src/App.js
+++ StockHut/StockHut/ClientApp/src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import Home from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
-import  Signup  from './components/Signup';
-import { NewsFeed }  from './components/NewsFeed';
-import './custom.css'
-
+import React, { Component } from "react";
+import { Route } from "react-router";
+import { Layout } from "./components/Layout";
+import Home from "./components/Home";
+import { FetchData } from "./components/FetchData";
+import { Counter } from "./components/Counter";
+import Signup from "./components/Signup";
+import { NewsFeed } from "./components/NewsFeed";
+import { ContactUs } from "./components/ContactUs";
+import { AboutUs } from "./components/AboutUs";
+import "./custom.css";
 
 export default class App extends Component {
   static displayName = App.name;
 
-      render () { 
-        return (
-           
-            <Layout>
-                <Route exact path='/' component={Home} />
-                <Route path='/signup' component={Signup} />
-                <Route path='/counter' component={Counter} />
-                <Route path='/fetch-data' component={FetchData} />
-                <Route path='/newsfeed' component={NewsFeed} />
-            </Layout>
-            
-        );
-      }
+  render() {
+    return (
+      <Layout>
+        <Route exact path="/" component={Home} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/counter" component={Counter} />
+        <Route path="/fetch-data" component={FetchData} />
+        <Route path="/newsfeed" component={NewsFeed} />
+        <Route path="/contactus" component={ContactUs} />
+        <Route path="/aboutus" component={AboutUs} />
+      </Layout>
+    );
+  }
 }
