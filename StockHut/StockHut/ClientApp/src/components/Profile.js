c23c5a53bde16bc6f509d1335a95d64f060ffe71
David Rossi <david.rossi101@gmail.com>
2020-04-06 15:19:04 -0400

    Cleaned up code

--- StockHut/StockHut/ClientApp/src/components/Profile.js
+++ StockHut/StockHut/ClientApp/src/components/Profile.js
@@ -27,40 +27,38 @@ function Copyright() {
   );
 }
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   icon: {
-    marginRight: theme.spacing(2)
+    marginRight: theme.spacing(2),
   },
   heroContent: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(2, 0, 3)
+    padding: theme.spacing(2, 0, 3),
   },
   heroButtons: {
-    marginTop: theme.spacing(4)
+    marginTop: theme.spacing(4),
   },
   cardGrid: {
     paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8)
+    paddingBottom: theme.spacing(8),
   },
   card: {
     height: "100%",
     display: "flex",
-    flexDirection: "column"
+    flexDirection: "column",
   },
   cardMedia: {
-    paddingTop: "56.25%" // 16:9
+    paddingTop: "56.25%", // 16:9
     //David: Change it to 100% to view
   },
   cardContent: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   footer: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6)
-  }
+    padding: theme.spacing(6),
+  },
 }));
-
-//const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function Profile() {
   const classes = useStyles();
@@ -70,11 +68,11 @@ export default function Profile() {
   const [caption, setCaption] = useState([]);
   const [count, setCount] = useState(0);
 
-  const fileSelectedHandler = event => {
+  const fileSelectedHandler = (event) => {
     setCards([URL.createObjectURL(event.target.files[0]), ...cards]);
   };
 
-  const inputHandler = event => {
+  const inputHandler = (event) => {
     setInput(event.target.value);
   };
 
@@ -136,7 +134,7 @@ export default function Profile() {
         <Container className={classes.cardGrid} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
-            {cards.map(card => (
+            {cards.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
                 <Card className={classes.card}>
                   <CardMedia
