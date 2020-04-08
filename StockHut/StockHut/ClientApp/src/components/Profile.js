094bb9741231b11d610540a7bac07e6ed403d377
Ayah <ayahshamma123@gmail.com>
2020-04-08 17:53:51 -0400

    Css change of user profile
    making the profile page match the rest of the web app

--- StockHut/StockHut/ClientApp/src/components/Profile.js
+++ StockHut/StockHut/ClientApp/src/components/Profile.js
@@ -17,48 +17,75 @@ import FavoriteIcon from "@material-ui/icons/Favorite";
 import CommentIcon from "@material-ui/icons/Comment";
 import { NavMenu } from "./Navbar/NavMenu";
 
+
 function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
       StockHut {new Date().getFullYear()}
-      {"."}
-    </Typography>
+          {"."}
+
+        </Typography>
+
   );
 }
-
+ 
 const useStyles = makeStyles((theme) => ({
+
+
   icon: {
-    marginRight: theme.spacing(2),
-  },
+      marginRight: theme.spacing(2),
+      width: '100%',
+    },
   heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(2, 0, 3),
+      backgroundColor: '#98ce7d',
+      
+    
   },
   heroButtons: {
-    marginTop: theme.spacing(4),
+      marginTop: theme.spacing(4),
+      align: 'right',
+      flexGrow: 1,
   },
-  cardGrid: {
+    cardGrid: {
+        backgroundColor: '#98ce7d',
     paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+        
+        
   },
-  card: {
-    height: "100%",
+    card: {
+        
+    
     display: "flex",
     flexDirection: "column",
   },
-  cardMedia: {
+    cardMedia: {
+     height: "100%",  
     paddingTop: "56.25%", // 16:9
     //David: Change it to 100% to view
   },
   cardContent: {
-    flexGrow: 1,
+      flexGrow: 1,
+      
   },
-  footer: {
+    footer: {
+       
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(6),
-  },
+    },
+    listText: {
+        fontfamily: [
+            'Gill Sans',
+            'Gill Sans MT',
+            'Calibri',
+            'Trebuchet MS',
+            'sans - serif'
+        ].join(','),
+    },
 }));
+
+
 
 export default function Profile() {
   const classes = useStyles();
@@ -81,35 +108,43 @@ export default function Profile() {
   };
 
   return (
-    <React.Fragment>
-      <CssBaseline />
+  
+        <div >
       <NavMenu />
-      <main>
+
+          <main>
         {/* Hero unit */}
-        <div className={classes.heroContent}>
-          <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="textPrimary"
-              gutterBottom
-            >
-              Name of User
+              <div className={classes.heroContent} >
+                  
+                  <Container>
+                      <div width="50%" display="flex">
+           <img src="https://cloudcone.com/wp-content/uploads/2019/03/blank-avatar.jpg" width="20%" height="30%"  />
+
+          <Typography
+          font= "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif" 
+          component="h1"
+          variant="h4"
+          align="left"
+          color="textPrimary"
+          gutterBottom
+        >
+          Adam21
             </Typography>
             <Typography
               variant="h5"
-              align="center"
+              align="left"
               color="textSecondary"
-              paragraph
+                          paragraph
+                          display="flex"
             >
               Description
-            </Typography>
-            <div className={classes.heroButtons}>
+            </Typography></ div>
+             <div className={classes.heroButtons} flexGrow="1" >
+
               <Grid container spacing={1} justify="center">
                 <Grid item>
-                  Post a Picture ---> &ensp;
-                  <input
+                  Post a Picture &ensp;                
+                 <input
                     type="file"
                     name="file"
                     placeholder="Upload Image"
@@ -117,7 +152,7 @@ export default function Profile() {
                   />
                   <br />
                   <br />
-                  Caption: &ensp;
+                  Caption: &ensp;                
                   <input
                     type="text"
                     id="caption"
@@ -125,13 +160,30 @@ export default function Profile() {
                     onChange={inputHandler}
                   />{" "}
                   &ensp;
-                  <button onClick={addCaption}>Submit</button>
-                </Grid>
+                
+                  <button onClick={addCaption}
+                    style={{ font: "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif ",
+                    text: 'uppercase',
+                    background: 'rgb(88, 165, 90)',
+                    width: '30%',    
+                    lineHeight: '20%',
+                        display: 'inline - block',
+                        padding: '4%',
+                        border: 'none',
+                    color: 'white',
+                    fontsize: '100%',
+                    cursor: 'pointer'}}>
+
+                   Submit </button>
+
+               </Grid>
               </Grid>
+            
             </div>
-          </Container>
+        </Container>
         </div>
-        <Container className={classes.cardGrid} maxWidth="md">
+
+        <Container className={classes.cardGrid} maxWidth="md" >
           {/* End hero unit */}
           <Grid container spacing={4}>
             {cards.map((card) => (
@@ -141,9 +193,9 @@ export default function Profile() {
                     className={classes.cardMedia}
                     image={card}
                     title="Image"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h12" component="h2">
+                        />
+                        <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h10" component="h4">
                       UserName
                     </Typography>
                     <Typography>
@@ -154,10 +206,12 @@ export default function Profile() {
                     <Button size="small" color="primary">
                       <FavoriteIcon onClick={() => setCount(count + 1)} />{" "}
                       {count}
+
                     </Button>
-                    <Button size="small" color="primary">
+                        <Button size="small" color="primary">
                       <CommentIcon />
                     </Button>
+
                   </CardActions>
                 </Card>
               </Grid>
@@ -165,8 +219,11 @@ export default function Profile() {
           </Grid>
         </Container>
       </main>
-      <Copyright />
-    </React.Fragment>
+
+
+          <Copyright />
+          
+       </div>
   );
 }
 
